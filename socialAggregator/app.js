47b0bfd9b041a40bbc43c8b51f545117fee0bd85
admin <admin@mac12.local>
2016-03-01 12:30:25 -0300

    Setup of google api credentials

--- socialAggregator/app.js
+++ socialAggregator/app.js
@@ -11,14 +11,17 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-passport.use(GoogleStrategy({
-    clientId: '',
-    clientSecret: '',
-    callbackURL: '', // Where google will send user back to when auth is done
-    function(req, accessToken, refreshToken, profile, done) { // When google sends something to callbackURL
-        done(null, profile);
-    }
-}));
+passport.use(GoogleStrategy(
+        {
+            clientID: '1026465773271-rmrnp1gqvj26k9qdgbo8g6c4nt8b8ni0.apps.googleusercontent.com',
+            clientSecret: 'EXaXNBcu-FKLiZeKB_Z4oWfk'
+            callbackURL: 'http://localhost:3000/auth/google/callback', // Where google will send user back to when auth is done
+        },
+        function(req, accessToken, refreshToken, profile, done) { // When google sends something to callbackURL
+            done(null, profile);
+        }
+    )
+);
 
 var app = express();
 
@@ -55,9 +58,9 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -65,23 +68,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
